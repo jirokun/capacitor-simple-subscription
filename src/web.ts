36ad4c167583591d240db8033288f241788aebfd
Jiro Iwamoto <jirokun@gmail.com>
2023-11-11 17:25:35 +0900

    inprogress

--- src/web.ts
+++ src/web.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { WebPlugin } from '@capacitor/core';
 
 import type { SubscriptionManagerPlugin } from './definitions';
@@ -6,13 +7,23 @@ export class SubscriptionManagerWeb
   extends WebPlugin
   implements SubscriptionManagerPlugin
 {
-  async subscribe(options: { productId: string }): Promise<void> {
-    console.log('Cannot use this method on web');
-  }
-  async hasValidSubscription(options: {
+  getSubscription(_options: {
     productId: string;
-  }): Promise<{ value: boolean }> {
-    console.log('Cannot use this method on web');
-    return { value: false };
+  }): Promise<{ subscription: { productId: string; expirationDate: Date } }> {
+    throw new Error('Method not implemented.');
+  }
+  showManageSubscriptions(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  restoreSubscription(_options: { productId: string }): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  async subscribe(_options: { productId: string }): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  hasValidSubscription(_options: {
+    productId: string;
+  }): Promise<{ hasValidSubscription: boolean }> {
+    throw new Error('Method not implemented.');
   }
 }
